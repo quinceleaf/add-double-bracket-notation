50efe03d61257e7439251c3826fe42d37723e336
Brian Ibbotson <brian@quinceleaf.dev>
2021-01-31 18:55:38 -0500

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,64 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log("Extension [[Add Double Bracket Notation to Selection]] is now active!");
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "extension.addBrackets",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // const addBrackets = () => {
+
+        // Get the active editor
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+          return; // No open editor
+        }
+  
+        const document = editor.document;
+        const selection = editor.selection;
+  
+        const textSelection = document.getText(selection);
+  
+        const regexPattern = /(.+)/g;
+  
+        const replacementCount = (str: string) => {
+          return ((str || "").match(regexPattern) || []).length;
+        };
+  
+        const formattedSelection = textSelection.replace(regexPattern, "[[$1]]");
+        const formattedCount = replacementCount(textSelection);
+  
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection, formattedSelection);
+        });
+  
+        // Display a status bar message to show progress
+        vscode.window.setStatusBarMessage("Converting ....");
+        vscode.window.setStatusBarMessage(
+          `Converted ${formattedCount} backlinks`,
+          2000
+        );
+  
+        // Display a message box to the user
+        vscode.window.showInformationMessage(
+          `Converted ${formattedCount} backlinks`
+        );
+      }
+    // }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
